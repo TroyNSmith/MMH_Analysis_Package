8552ce37fa1054139c86911a77a93495c0e86497
Troy Smith <Troy.Smith@uga.edu>
2025-07-23 12:58:56 -0400

    Testing

--- src/test.py
+++ src/test.py
@@ -1,12 +1,10 @@
 import click
-from pathlib import Path
 from util.traj import reader, selections
 from traj import rdf
 
 # Test files
-DATA_DIR = Path("tests/adk_oplsaa")
-GRO_FILE = DATA_DIR / "adk_oplsaa.gro"
-XTC_FILE = DATA_DIR / "adk_oplsaa.xtc"
+GRO_FILE = "tests/adk_oplsaa/adk_oplsaa.gro"
+XTC_FILE = "tests/adk_oplsaa/adk_oplsaa.xtc"
 
 
 @click.group()
@@ -36,18 +34,18 @@ def analyze():
     help="Selection string for second reference group in analyses.",
 )
 def bulk(
-    Topology: str, Trajectory: str, Minimal: bool, Group1: str, Group2: str = None
+    topology: str, trajectory: str, minimal: bool, group1: str, group2: str = None
 ):
     """
     Test function to load a trajectory.
     :param topology: Path string to topology file.
     :param trajectory: Path string to trajectory file.
     :param minimal: (False) Whether to run minimal analysis.
-    :param group1: Selection string for first reference group.
+    :param group2: Selection string for first reference group.
     :param group2: Selection string for second reference group.
     """
-    Universe = reader.load_traj(Trajectory, Topology)
-    Pairs = selections.select_pairs(Universe, Group1, Group2)
+    Universe = reader.load_traj(trajectory, topology)
+    Pairs = selections.select_pairs(Universe, group1, group2)
     R, G_R = rdf.rdf(Universe, Pairs)
     
 
