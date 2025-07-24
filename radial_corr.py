a76ede523154807d51c2c051db791f71330f56b6
Troy Smith <Troy.Smith@uga.edu>
2025-07-24 14:48:43 -0400

    Radial correlation function

--- radial_corr.py
+++ radial_corr.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib as plt
 
 testDir ="/media/mmh/ExtraSpace/Final_Pore_Analysis_Organized_TNS/pore_D3_L6_W2_S5.0_E0.0_A0.0_V0.0_no_reservoir_N1/OCT/328K/5_nvt_prod_system"
+rOuter = 1.2
+rInner = 0.5
 
 mdeTraj = mde.open(testDir, "run.tpr", "out/traj.xtc", nojump=True)
 
@@ -15,3 +17,7 @@ score_list = [[] for _ in range(n_atoms)]
 for frame in octTraj.frames:
     octPos = frame.positions - frame._unitcell[:3] / 2
     octRad = np.linalg.norm(octPos[:,:2], axis=1)
+
+    for i, r in enumerate(octRad):
+        if not tracking[i]:
+            if r > rOuter:
